b42fe4fa157789d359577c1e073f68ebf83222ee
Mohcine Ait Taleb <mait-tal@e1r10p15.1337.ma>
2026-06-05 21:09:50 +0100

    feature: basic structs

--- includes/codexion.h
+++ includes/codexion.h
@@ -1,4 +1,28 @@
 #ifndef CODEXION_H
 # define CODEXION_H
 
+typedef enum e_coder_state
+{
+	IDLE,
+	WORKING
+}	t_coder_state;
+
+typedef struct s_coder
+{
+	int				id;
+	t_coder_state	state;
+}	t_coder;
+
+typedef struct s_resource
+{
+	int				owner_id;
+}	t_resource;
+
+typedef struct s_simulation
+{
+	int				running;
+	t_coder		*coders;
+	t_resource		resource;
+}	t_simulation;
+
 #endif /* CODEXION_H */
