c8f6bcf6e93bdc1f802ed5aaa51b5b239244220e
Mohcine Ait Taleb <mait-tal@e2r10p15.1337.ma>
2026-06-19 21:13:42 +0100

    add heap mecanisme to take dongles

--- src/dongle.c
+++ src/dongle.c
@@ -7,6 +7,7 @@
 static int	can_take(t_coder *coder);
 static void	lock_dongles(t_coder *coder);
 static void	unlock_dongles(t_coder *coder);
+static void	add_request(t_coder *coder);
 
 int	init_dongles(t_simulation *sim)
 {
@@ -33,11 +34,14 @@ int	take_dongles(t_coder *coder)
 		smart_sleep(coder->sim, (coder->sim->config.time_to_compile + coder->sim->config.dongle_cooldown) / 4);
 
 	//request_dongles();
+	add_request(coder);
 
-	while (!can_take(coder))
+	while (!can_take(coder) && !simulation_stopped(coder->sim))
 		//use smart sleep for sim stop detection
 		//if sim stoped return
 		usleep(1000);
+	if (simulation_stopped(coder->sim))
+		return (0);
 
 	lock_dongles(coder);
 	coder->left_dongle->available = 0;
@@ -54,6 +58,9 @@ void	release_dongles(t_coder *coder)
 	if (!coder->has_dongles)
 		return ;
 	lock_dongles(coder);
+	heap_remove_top(&coder->left_dongle->waiters);
+	heap_remove_top(&coder->right_dongle->waiters);
+	coder->requested = 0;
 	coder->left_dongle->available = 1;
 	coder->right_dongle->available = 1;
 	coder->has_dongles = 0;
@@ -64,6 +71,16 @@ void	release_dongles(t_coder *coder)
 static int	can_take(t_coder *coder)
 {
 	lock_dongles(coder);
+	if (heap_top(&coder->left_dongle->waiters) != &coder->request)
+	{
+		unlock_dongles(coder);
+		return (0);
+	}
+	if (heap_top(&coder->right_dongle->waiters) != &coder->request)
+	{
+		unlock_dongles(coder);
+		return (0);
+	}
 	if (!coder->left_dongle->available || !coder->right_dongle->available)
 	{
 		unlock_dongles(coder);
@@ -99,4 +116,23 @@ static void	unlock_dongles(t_coder *coder)
 		pthread_mutex_unlock(&coder->left_dongle->mutex);
 		pthread_mutex_unlock(&coder->right_dongle->mutex);
 	}
+}
+
+static void	add_request(t_coder *coder)
+{
+	lock_dongles(coder);
+	if (coder->sim->config.scheduler_type == SCHEDULER_FIFO)
+		coder->request.priority = get_sim_time(coder->sim);
+	else
+		coder->request.priority = coder->last_compile_time_ms
+			+ coder->sim->config.time_to_burnout;
+	if (!coder->requested)
+	{
+		heap_insert(&coder->left_dongle->waiters,
+			&coder->request);
+		heap_insert(&coder->right_dongle->waiters,
+			&coder->request);
+		coder->requested = 1;
+	}
+	unlock_dongles(coder);
 }
